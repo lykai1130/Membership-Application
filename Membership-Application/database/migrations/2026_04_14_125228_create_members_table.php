645773696ef694a27e3da86d9ef235941cc38ae4
YeeKai <laiyeekai@gmail.com>
2026-04-14 21:37:10 +0800

    migration

---
+++ Membership-Application/database/migrations/2026_04_14_125228_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->date('dob');
+            $table->char('gender', 1);
+            $table->foreignId('referral_id')->nullable()->constrained('members')->nullOnDelete();
+            $table->string('referral_code')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
